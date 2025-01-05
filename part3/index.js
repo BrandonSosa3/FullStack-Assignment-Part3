5c497d22e16bd3d86e20c502a6e0e78eeabb4d8b
bsosa3 <157088552+bsosa3@users.noreply.github.com>
2025-01-04 16:33:00 -0800

    more changes

--- part3/index.js
+++ part3/index.js
@@ -14,17 +14,7 @@ app.use(cors())
 // If a file is found, Express will return it. 
 app.use(express.static('dist'))
 
-const morgan = require('morgan')
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
-
-app.use(requestLogger)
 
 let persons = [
     {
@@ -50,11 +40,20 @@ let persons = [
 ]
 
 app.use(express.json()) 
-app.use(express.urlencoded({ extended: true }))
 
-morgan.token('body', (req) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
+app.use(requestLogger)
+
+app.get('/', (request, response) => {
+  response.send('<h1>This Server is Fire and Gang</h1>')
+})
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
@@ -125,6 +124,9 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
 
+app.use(unknownEndpoint) 
+
+
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
@@ -137,8 +139,6 @@ const errorHandler = (error, request, response, next) => {
     next(error)
 }
   
-// Middleware
-app.use(unknownEndpoint)  
 // this has to be the last loaded middleware, also all the routes should be registered before this!
 app.use(errorHandler)
 
